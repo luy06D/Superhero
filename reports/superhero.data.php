53b75a9952e4fb7f84ca701157a03c346ab090da
luy06D <cusiluis04@gmail.com>
2023-05-15 13:41:29 -0500

    Avance del generador de reporte

---
+++ reports/superhero.data.php
@@ -0,0 +1,42 @@
+<h1 class="text-center text-md">Reporte de superheroes</h1>
+
+<table class="table table-border mt-5">
+  <colgroup>
+    <col style='width: 5%'>
+    <col style='width: 35%'>
+    <col style='width: 15%'>
+    <col style='width: 25%'>
+    <col style='width: 10%'>
+    <col style='width: 10%'>
+
+
+  </colgroup>
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>Nombre</th>
+      <th>Género</th>
+      <th>Raza</th>
+      <th>Bando</th>
+      <th>Estatura</th>
+    </tr>
+
+  </thead>
+  <tbody>
+
+  <?php foreach($datos as $registro): ?>
+    <tr>
+      <td><?=$registro['id']?></td>
+      <td><?=$registro['superhero_name']?></td>
+      <td><?=$registro['gender']?></td>
+      <td><?=$registro['race']?></td>
+      <td><?=$registro['alignment']?></td>
+      <td><?=$registro['height_cm']?></td>
+  
+      
+    </tr>
+  
+  <?php endforeach; ?>
+
+  </tbody>
+</table>
---
+++ reports/superhero.report.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once '../vendor/autoload.php';
+require_once '../model/SuperHeroe.php';
+
+use Spipu\Html2Pdf\Html2Pdf;
+use Spipu\Html2Pdf\Exception\Html2PdfException;
+use Spipu\Html2Pdf\Exception\ExceptionFormatter;
+
+try {
+
+    //Instanciamos la clase SuperHero
+    $superhero = new SuperHeroe();
+    $datos = $superhero->listarSuperHeroes(3);
+
+    ob_start();
+
+    include './estilos.report.html';
+    include './superhero.data.php';
+
+  
+ 
+    $content = ob_get_clean();
+
+    $html2pdf = new Html2Pdf('P', 'A4', 'es');
+    $html2pdf->writeHTML($content);
+    $html2pdf->output('SuperHero.pdf');
+} catch (Html2PdfException $e) {
+    $html2pdf->clean();
+
+    $formatter = new ExceptionFormatter($e);
+    echo $formatter->getHtmlMessage();
+}
